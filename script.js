a2a4fcd8ee0fc9b1f23340e812dbf7753ebc6ef7
rohannj1993 <rohannj1993@gmail.com>
2021-05-19 18:48:56 -0400

    added div tags so that hours are not within each other

--- script.js
+++ script.js
@@ -2,10 +2,11 @@ $( document ).ready(function() {
  $("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'))
 
 
+ var now = moment();
+ var time = now.hour() ;
 
 
-
-
+ console.log (time)
 
 
 
