3d73ca16e9e3fc24a602d9a6783d53b4909e3b36
rohannj1993 <rohannj1993@gmail.com>
2021-05-23 14:07:20 -0400

    color of hours changes

--- script.js
+++ script.js
@@ -1,38 +1,42 @@
-$( document ).ready(function() {
- $("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'))
+$(document).ready(function () {
+    $("#currentDay").text(moment().format('dddd, MMMM Do, YYYY'))
 
 
- var now = moment();
- var time = now.hour() ;
+    var now = moment();
+    var time = now.hour();
 
 
- console.log (time)
-
-
-
+    console.log(time)
 
 
 
+    function hourUpdate() {
 
+        var now = new Date().getHours();
+        console.log(now)
 
+        $(".time-block").each(function () {
+            var rowHour = parseInt($(this).attr("id"))
+            console.log(rowHour)
 
+            if (now > rowHour) {
+                $(this).removeClass("future");
+                $(this).addClass("past");
+            }
+            else if (now === rowHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+            }
 
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
 
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- } ) ;
+        )
+    } 
+    hourUpdate()
+})
